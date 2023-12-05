8a4f7d5ceeb94cf418a0210647f52b0ec004110c
Tejas Redkar <redkartejas213@gmail.com>
2023-12-05 19:44:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-
+from nltk import punkt
+nltk.download('punkt')
+nltk.download('stopwords')
 ps = PorterStemmer()
 
 
